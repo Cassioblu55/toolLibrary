b933f7b4258fe59ef82c12809542530e99b8be48
Cassio Hudson <cassioblubyrd@gmail.com>
2016-08-05 14:03:56 -0500

    updated error display reference to use specific version of error display

--- resources/templates/head.php
+++ resources/templates/head.php
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" type="text/css"  href="<?php echo $baseURL;?>resources/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="icon" href="<?php echo $baseURL;?>resources/icons/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="<?php echo $baseURL; ?>resources/layout.css"/>
-	<script src="http://www.cassiohudson.com/utilities/js/errorDisplay.js"></script>
+	<script src="http://www.cassiohudson.com/utilities/js/errorDisplay/ed_1_0_0.js"></script>
 </head>
 <script>var baseURL="<?php echo $baseURL;?>"</script>
 
